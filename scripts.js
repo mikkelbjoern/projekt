cfa320b3bc0601c898da36d26ba8104f10a4b72b
Sebastian <Sebastianhansen1997@hotmail.com>
2017-01-16 14:15:41 +0100

    added copper, silver & gold coins

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 let money = 0;
+let worker = [];
+
 let clickButton = document.getElementById('clickButton');
 
 let updateMoney = function (){
-  document.getElementById('moneyField').innerHTML = "Money: " + money;
+  document.getElementById('moneyField').innerHTML = "Copper coins: " + money;
 }
 
 let moneyClick = function(){
   money += 1;
   updateMoney();
-
 }
